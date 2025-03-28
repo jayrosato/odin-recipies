5a3fbaea21f8359dcf5b78f40db5cfd065b8077c
Jay Rosato <rosato.jay@gmail.com>
2025-03-28 09:28:52 +0200

    Updated Object Construction
    Same basic code, but I made some slight changes to how the objects were constructed and
    tried my best to implement factory constriction.

--- TicTacToe/scripts.js
+++ TicTacToe/scripts.js
@@ -1,11 +1,23 @@
-function Board(dimension, players) {
-   this.dimension = dimension,
-    this.players = players,
-    this.current_player = this.players[0];
-    this.squares = [];
-    this.printBoard =  function () {
+const players = [];
+
+function createUser (name) {return {name};};
+
+function createPlayer(name, mark) {
+    const { }= createUser(name);
+    return {name, mark};
+};
+
+players.push(createPlayer('Me', 'X'))
+players.push(createPlayer('You', 'O'))
+
+const board = (function () {
+    let currentPlayer = players[0];
+    const dimension = 3;
+    let squares = [];
+    
+    const printBoard =() => {
         let index=0;
-        board= document.getElementById("board");
+        boardContainer= document.getElementById("board");
         for(let y=1; y<=dimension; y++){
         let row = document.createElement("div");
         row.setAttribute("class", 'row');
@@ -13,76 +25,70 @@ function Board(dimension, players) {
                 space=document.createElement("div");
 
                 const square = {index: index, x: x, y: y, mark: 'empty'};
-                this.squares.push(square);
+                squares.push(square);
 
                 space.setAttribute("id", index);
                 space.setAttribute("class", 'space');
                 space.innerHTML = "Empty";
-                space.onclick = this.turn.bind(this, index)
-                row.appendChild(space);
+                space.onclick = ((id)=>()=>turn(id))(index);
+                row.appendChild(space);;
                 index+=1};
-
-            board.appendChild(row);
+                boardContainer.appendChild(row);
             };
     };
 
-    this.turn = function (id) {
-        this.squares[id].mark =this.current_player.mark;
-        document.getElementById(id).innerHTML= this.current_player.mark;
+    const turn = (id) => {
+        console.log(id)
+        squares[id].mark =currentPlayer.mark;
+        document.getElementById(id).innerHTML= currentPlayer.mark;
         document.getElementById(id).onclick = ""
-        this.checkVictory(id);
-        if(this.current_player == this.players[0]) {this.current_player = this.players[1]}
-        else {this.current_player=this.players[0]};
+        checkVictory(id);
+        if(currentPlayer == players[0]) {currentPlayer = players[1]}
+        else {currentPlayer=players[0]};
     };
 
-    this.checkVictory = function (id) {
-        for(let i=0; i<this.squares.length; i++){console.log(i);
-            if(this.squares[i].mark == 'empty') {break};
-            if(i==this.squares.length-1){console.log(`Game is a tie!`);};
+    const checkVictory = (id) => {
+        for(let i=0; i<squares.length; i++){console.log(i);
+            if(squares[i].mark == 'empty') {break};
+            if(i==squares.length-1){console.log(`Game is a tie!`);};
         };
 
-        if(id==0||id==3||id==6) {if(this.squares[id+1].mark==this.current_player.mark && 
-            this.squares[id+2].mark==this.current_player.mark){
-                {console.log(`${this.current_player.name} wins a vertical victory!`)};
+        if(id==0||id==3||id==6) {if(squares[id+1].mark==currentPlayer.mark && 
+            squares[id+2].mark==currentPlayer.mark){
+                {console.log(`${currentPlayer.name} wins a vertical victory!`)};
             }}
-        else if (id==1||id==4||id==7) {if(this.squares[id-1].mark==this.current_player.mark && 
-            this.squares[id+1].mark==this.current_player.mark){
-                console.log(`${this.current_player.name} wins a vertical victory!`)}}
+        else if (id==1||id==4||id==7) {if(squares[id-1].mark==currentPlayer.mark && 
+            squares[id+1].mark==currentPlayer.mark){
+                console.log(`${currentPlayer.name} wins a vertical victory!`)}}
 
         else if(id==2||id==5||id==8) {
-            if(this.squares[id-1].mark==this.current_player.mark && 
-            this.squares[id-2].mark==this.current_player.mark){
-                console.log(`${this.current_player.name} wins a vertical victory!`)}}
+            if(squares[id-1].mark==currentPlayer.mark && 
+            squares[id-2].mark==currentPlayer.mark){
+                console.log(`${currentPlayer.name} wins a vertical victory!`)}}
 
-        if(id==0||id==1||id==2) {if(this.squares[id+3].mark==this.current_player.mark && 
-            this.squares[id+6].mark==this.current_player.mark){
-                {console.log(`${this.current_player.name} wins a horizontal victory!`)};
+        if(id==0||id==1||id==2) {if(squares[id+3].mark==currentPlayer.mark && 
+            squares[id+6].mark==currentPlayer.mark){
+                {console.log(`${currentPlayer.name} wins a horizontal victory!`)};
             }}
-        else if (id==3||id==4||id==5) {if(this.squares[id-3].mark==this.current_player.mark && 
-            this.squares[id+3].mark==this.current_player.mark){
-                console.log(`${this.current_player.name} wins a horizontal victory!`)}}
+        else if (id==3||id==4||id==5) {if(squares[id-3].mark==currentPlayer.mark && 
+            squares[id+3].mark==currentPlayer.mark){
+                console.log(`${currentPlayer.name} wins a horizontal victory!`)}}
 
         else if(id==6||id==7||id==8){
-            if(this.squares[id-3].mark==this.current_player.mark && 
-            this.squares[id-6].mark==this.current_player.mark){
-                console.log(`${this.current_player.name} wins a horizontal victory!`)}}
+            if(squares[id-3].mark==currentPlayer.mark && 
+            squares[id-6].mark==currentPlayer.mark){
+                console.log(`${currentPlayer.name} wins a horizontal victory!`)}}
         
-        if(this.squares[0].mark==this.current_player.mark &&
-            this.squares[4].mark==this.current_player.mark &&
-            this.squares[8].mark==this.current_player.mark)
-            {console.log(`${this.current_player.name} wins a diagnol victory!`)}
-        else if (this.squares[2].mark==this.current_player.mark &&
-            this.squares[4].mark==this.current_player.mark &&
-            this.squares[6].mark==this.current_player.mark)
-            {console.log(`${this.current_player.name} wins a diagnol victory!`)}
-    }
+        if(squares[0].mark==currentPlayer.mark &&
+            squares[4].mark==currentPlayer.mark &&
+            squares[8].mark==currentPlayer.mark)
+            {console.log(`${currentPlayer.name} wins a diagnol victory!`)}
+        else if (squares[2].mark==currentPlayer.mark &&
+            squares[4].mark==currentPlayer.mark &&
+            squares[6].mark==currentPlayer.mark)
+            {console.log(`${currentPlayer.name} wins a diagnol victory!`)}
+    };
+    return{dimension, players, squares, currentPlayer, printBoard, turn, checkVictory}
+})();
 
-};
-
-
-const player1 = {name: 'Me', mark: 'X'}
-const player2 = {name: 'You', mark: 'O'}
-players = [player1, player2]
-
-const grid = new Board(3, players);
-grid.printBoard();
+board.printBoard()
